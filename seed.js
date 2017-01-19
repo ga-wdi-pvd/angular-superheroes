91a3e114e6e4b04f9de9030b91740302211790fe
murphypicard <ayaghsizian@gmail.com>
2017-01-19 09:00:32 -0500

    working on the submit form

---
+++ seed.js
@@ -0,0 +1,6 @@
+let heroes = {
+  ara: {name: "Ara", phone: 1234567890, comics: 'aomics stuff', image: 'ahe image'},
+  bob: {name: "Bob", phone: 2234567890, comics: 'bomics stuff', image: 'bhe image'},
+  cam: {name: "Cam", phone: 3234567890, comics: 'comics stuff', image: 'che image'},
+  del: {name: "Del", phone: 4234567890, comics: 'domics stuff', image: 'dhe image'}
+};
